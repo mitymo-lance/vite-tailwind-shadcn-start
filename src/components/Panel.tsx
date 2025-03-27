b7abb9b21c87f9c0abdeee668befa4e0c6d4def8
Lance Eppley <lance@mitymo.com>
2025-03-27 15:36:52 -0400

    added className has a prop to Panel

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -2,16 +2,13 @@ interface PanelProps {
   children: React.ReactNode
   bgColor?: string
   width?: string
+  className?: string
 }
 
-
-export default function Panel({ children, bgColor = 'white', width = 'full' }: PanelProps) {
-  
-
+export default function Panel({ children, bgColor = 'white', width = 'full', className = '' }: PanelProps) {
   return (
-    <div className={`mr-panel bg-${bgColor} rounded-lg shadow-md p-6 space-y-6 mx-2 mb-6 justify-items-center w-${width}`}>
+    <div className={`mr-panel bg-${bgColor} rounded-lg shadow-md p-6 space-y-6 mx-2 mb-6 justify-items-center w-${width} ${className}`}>
       {children}
-
     </div>
   )
 }
